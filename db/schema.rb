db65302f752bdd31841c7972fc43234fab96e070
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2018-08-02 17:37:00 +0300

    User edit page added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_131512) do
+ActiveRecord::Schema.define(version: 2018_08_02_140913) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2018_08_02_131512) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "phonenumber"
+    t.string "job"
+    t.string "gender"
+    t.integer "age"
+    t.text "reference_note"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

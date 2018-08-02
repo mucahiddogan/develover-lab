161a8f6736f6b6489b38e77eee3eb46c9be13af7
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2018-08-02 18:08:20 +0300

    attribute save bug fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up , keys:[:firstname, :email, :password,:lastname,:phonenumber,:job,:gender,:age,:reference_note] )
+            devise_parameter_sanitizer.permit(:account_update , keys:[:firstname, :email, :password , :current_password,:lastname,:phonenumber,:job,:gender,:age,:reference_note])
+        end
+
 end

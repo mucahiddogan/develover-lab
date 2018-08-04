ae7453aa78409a7accb9154f6ba30aa2fe2db662
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2018-08-04 10:43:51 +0300

    last changes

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -8,8 +8,6 @@ class PartnersController < ApplicationController
 	def vote_inc
 		@user = User.find(params[:id])
 		@user.liked_by current_partner
-		p "----------"
-		p @user.votes_for.size
 
 		status(@user)
 
@@ -21,20 +19,26 @@ class PartnersController < ApplicationController
 		total = user.votes_for.size
 		if (total > (@partners.count + 1)/2)
 			user.status = true
-			user.card_number= user.id + 1000000
+			user.card_number = user.id + 1000000
+			user.referance_id=random_string
 			user.save
 		end
+		random_string
 	end
 
 	def vote_dec
 		@user = User.find(params[:id])
 		@user.unliked_by current_partner
-		p "----------"
-		p @user.votes_for.size
 
 		status(@user)
 
 		redirect_to partner_path
+	end
+
+	def random_string
+		o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+		string = (0...10).map { o[rand(o.length)] }.join
+		return string
 	end
 
 

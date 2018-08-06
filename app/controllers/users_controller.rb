cdc1b682530cca223589c6df3d3c67bb223ff90e
nafidurmus <nafidurmus07@gmail.com>
2018-08-06 11:15:05 +0300

    Update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 		@user =  User.all
 		#@user = User.find(params[:id])
 		#puts @user.email
-		@qr = RQRCode::QRCode.new("asdfhgash")
+		@qr = RQRCode::QRCode.new("asdfhgash") #referance code daki değer alınacak.
      	#@user = User.find(params[:id])
      	#@qr = RQRCode::QRCode.new(@user.provisioning_uri("Develoverteam"), :size => 7, :level => :h )
    end
